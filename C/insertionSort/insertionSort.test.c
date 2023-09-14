3eb57620087f874b456fdedb03470c4ebde9c546
Jacob Thompson <jacobjake683@gmail.com>
2023-09-13 19:23:12 -0500

    Add some test cases to C insertion sort

--- C/insertionSort/insertionSort.test.c
+++ C/insertionSort/insertionSort.test.c
@@ -1,6 +1,7 @@
 #define TEST
 #include <assert.h>
 #include "insertionSort.h"
+#include "stdio.h"
 
 
 //Returns 1 on array lengths not equal
@@ -22,16 +23,37 @@ int main() {
 	int testA[2] = {1, 2};
 	int correctA[2] = {1, 2};
 	insertionSort(testA, 2);
+	assert(compare_arrays(testA, 2, correctA, 2) == 0);
 
 	int testB[2] = {2, 1};
 	int correctB[2]  = {1, 2};
 	insertionSort(testB, 2);
+	assert(compare_arrays(testB, 2, correctB, 2) == 0);
 
 	int testC[7] = {8, 6, 7, 5, 3, 0, 9};
 	int correctC[7] = {0, 3, 5, 6, 7, 8, 9};
 	insertionSort(testC, 7);
-
-	assert(compare_arrays(testA, 2, correctA, 2) == 0);
-	assert(compare_arrays(testB, 2, correctB, 2) == 0);
 	assert(compare_arrays(testC, 2, correctC, 2) == 0);
+
+	int testD[9] = {4093, 3012, 3333, 1930, 0, 310, 100, 2, 100000};
+	int correctD[9] = {0, 2, 100, 310, 1930, 3012, 3333, 4093, 100000};
+	insertionSort(testD, 9);
+	assert(compare_arrays(testD, 9, correctD, 9) == 0);
+
+	int testE[10] = {8000, 1000, 7000, 6000, 5000, 4000, 3000, 1000, 2000, 50};
+	int correctE[10] = {50, 1000, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000};
+	insertionSort(testE, 10);
+	assert(compare_arrays(testE, 10, correctE, 10) == 0);
+
+	int testF[5] = {-1, -2, -3, -4, -5};
+	int correctF[5] = {-5, -4, -3, -2, -1};
+	insertionSort(testF, 5);
+	assert(compare_arrays(testF, 5, correctF, 5) == 0);
+
+	int testG[5] = {0, 0, 0, 0, 0};
+	int correctG[5] = {0, 0, 0, 0, 0};
+	insertionSort(testG, 5);
+	assert(compare_arrays(testG, 5, correctG, 5) == 0);
+
+	printf("6 tests passed.\n");
 }
