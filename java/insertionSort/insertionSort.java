eec9bd5332c0670a1eafa3dff7ce28f0cc273cc9
Jacob Thompson <jacobjake683@gmail.com>
2023-09-15 17:23:39 -0500

    Don't need to return a reference

--- java/insertionSort/insertionSort.java
+++ java/insertionSort/insertionSort.java
@@ -1,28 +1,26 @@
 import java.util.Arrays;
 
 class insertionSort {
-	public static int[] swap(int[] array, int a, int b) {
+	public static void swap(int[] array, int a, int b) {
 		int tmp = array[a];
 		array[a] = array[b];
 		array[b] = tmp;
-		return array;
 	}
 
-	public static int[] insertionSortInt(int[] array) {
+	public static void insertionSortInt(int[] array) {
 		int i = 1;
 		while(i < array.length) {
 			int j = i;
 			while(j > 0 && array[j-1] > array[j]) {
-				array = swap(array, j, j-1);
+				swap(array, j, j-1);
 			}
 			i++;
 		}
-		return array;
 	}
 
 	public static void main(String[] args) {
 		int[] a = {2, 1};
-		int[] aSorted = insertionSortInt(a);
-		System.out.println(Arrays.toString(aSorted));
+		insertionSortInt(a);
+		System.out.println(Arrays.toString(a));
 	}
 }
