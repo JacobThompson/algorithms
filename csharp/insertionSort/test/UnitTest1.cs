bc833a0d6cd245db6ebc9542162d27a42c2e7ef6
Jacob Thompson <jacobjake683@gmail.com>
2023-09-22 16:24:32 -0500

    Unit tests.

--- csharp/insertionSort/test/UnitTest1.cs
+++ csharp/insertionSort/test/UnitTest1.cs
@@ -11,8 +11,17 @@ public class UnitTest1
 
 		double[] testA = {1, 2, 3};
 		double[] correctA = {1, 2, 3};
-		IS.Sort(A);
+		IS.Sort(testA);
+		Assert.Equal(testA, correctA);
 
+		double[] testB = {3, 2, 1};
+		double[] correctB = {1, 2, 3};
+		IS.Sort(testB);
+		Assert.Equal(testB, correctB);
 
+		double[] testC = {8, 6, 7, 5, 3, 0, 9};
+		double[] correctC = {0, 3, 5, 6, 7, 8, 9};
+		IS.Sort(testC);
+		Assert.Equal(testC, correctC);
 	}
 }
